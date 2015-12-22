6280e830672a0fe7ab84f73921cafa320ef68c60
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-12-22 10:32:41 +0000

    pytest in requirements-test

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 package = 'aws-utils'
 version = '0.1'
 
+# TODO: find out why with tox the requirements file is not found
 # REQ_FILE = path.join(path.dirname(__file__), 'requirements.txt')
 # REQUIREMENTS = [x.strip() for x in open(REQ_FILE)]
 
@@ -13,7 +14,7 @@ setup(
     version=version,
     description="collection of AWS useful functions",
     url='https://github.com/skimhub/aws-utils',
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(),
     package_data={'': ['requirements.txt']},
     # install_requires=REQUIREMENTS,
 )
