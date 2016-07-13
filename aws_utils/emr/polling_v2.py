5f8555839069d2de6e7e5913d80cf923a9503b7b
Eddie Berbis <eddie@skimlinks.com>
2016-07-13 18:53:03 +0100

    Implement Cluster Status Polling - EMR
    This commit proposes a solution to wait until a cluter finishes all
    its steps, fails or takes longer than expected to finish.

---
+++ aws_utils/emr/polling_v2.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import logging
+import time
+from collections import namedtuple
+from datetime import datetime, timedelta
+
+from dateutil.tz import tzlocal
+
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+
+States = namedtuple('States', ['running', 'success', 'failure'])
+
+CLUSTER_STATES = States(
+    ['STARTING', 'BOOTSTRAPPING', 'RUNNING', 'WAITING', 'TERMINATING'],
+    ['TERMINATED'],
+    ['TERMINATED_WITH_ERRORS'])
+
+
+class EMRPollingException(Exception):
+    pass
+
+def on_cluster_status_change(logger, status, prev_state):
+    """Handle EMR cluster status change."""
+    logger.debug('Cluster status changed: %s -> %s', prev_state, status)
+
+
+def on_cluster_success_status(logger, status, reason):
+    """Handle EMR cluster success status."""
+    logger.debug('Cluster success: %s %s', status, reason)
+
+
+def on_cluster_failure_status(logger, status, reason):
+    """Handle EMR cluster failure status."""
+    msg = 'Cluster failure: {} {}'.format(status, reason)
+    logger.debug(msg)
+    raise EMRPollingException(msg)
+
+
+def has_reached_wait_limit(logger, status, wait_limit):
+    '''
+    :param status: dict containing `Timeline` key with cluster's `CreationDateTime`
+    :param wait_limit: limit to wait for the cluster to finish its job
+    :return: a tuple boolean to consider whether the polling for the cluster
+    status should be ended or not.
+    '''
+    stop = False
+    start = status['Timeline']['CreationDateTime']
+    now = datetime.now(tzlocal())
+    elapsed_time = now - start
+
+    if elapsed_time.total_seconds() > wait_limit:
+        msg = ('This job has been running for over {}. '
+               'Considering it a failure since the wait limit is {}'
+               ''.format(timedelta(seconds=elapsed_time.total_seconds()),
+                         timedelta(seconds=wait_limit)))
+        stop = True
+        logger.warning(msg)
+
+    return stop
+
+
+def has_cluster_successfully_finished(emr, cluster_id, logger=LOGGER,
+                                      on_status_change=on_cluster_status_change,
+                                      on_success=on_cluster_success_status,
+                                      on_failure=on_cluster_failure_status,
+                                      wait_duration=30, wait_limit=86400):
+    '''
+    :param emr: botocore.client.EMR object
+    :param logger: logger to log
+    :param cluster_id: cluter ID on AWS
+    :param on_status_change: action to perform on status change
+    :param on_success: action to perform on cluster success
+    :param on_failure: action to perform on cluster failure
+    :param wait_duration: delay in between checks
+    :param wait_limit: total limit to wait for cluster termination. If
+    overflow, then raises an EMRPollingException
+    :return: True if success, False if any kind of failure.
+    '''
+
+    prev_state = None
+    while True:
+        try:
+            description = emr.describe_cluster(ClusterId=cluster_id)
+
+            status = description['Cluster']['Status']
+
+            if status['State'] != prev_state:
+                on_status_change(logger, status['State'], prev_state)
+                prev_state = status['State']
+
+            if status['State'] in CLUSTER_STATES.success:
+                on_success(logger, status['State'], status['StateChangeReason'])
+                return True
+
+            elif status['State'] in CLUSTER_STATES.failure:
+                on_failure(logger, status['State'], status['StateChangeReason'])
+                return False
+
+            if has_reached_wait_limit(logger, status, wait_limit):
+                raise EMRPollingException('Cluster is taking too much time')
+            else:
+                time.sleep(wait_duration)
+        except Exception as exc:
+            logger.exception('Unable to get cluster status')
+            raise EMRPollingException(exc)
---
+++ tests/unit/test_polling_v2.py
@@ -0,0 +1,95 @@
+import datetime
+
+from dateutil.tz import tzlocal
+from mock import MagicMock
+from pytest import raises
+
+from aws_utils.emr.polling_v2 import (has_cluster_successfully_finished,
+                                      EMRPollingException)
+
+TEST_CLUSTER_ID = 'A Cluster ID'
+
+RUNNING_CLUSTER_STATUS_RESPONSE = {
+    u'Cluster': {
+        u'Id': u'A Cluster ID',
+        u'Status': {
+            u'State': u'RUNNING',
+            u'StateChangeReason': {
+                u'Message': u'Running step'
+            },
+            u'Timeline': {
+                u'CreationDateTime': datetime.datetime(2000, 7, 13, 11, 34, 21, 919000, tzinfo=tzlocal()),
+                u'ReadyDateTime': datetime.datetime(2016, 7, 13, 11, 44, 48, 973000, tzinfo=tzlocal())
+            }
+        }
+    }
+}
+
+
+TERMINATED_CLUSTER_STATUS_RESPONSE = {
+    u'Cluster': {
+        u'Id': u'A Cluster ID',
+        u'Status': {
+            u'State': u'TERMINATED',
+            u'StateChangeReason': {
+                u'Code': u'ALL_STEPS_COMPLETED',
+                u'Message': u'Steps completed'
+            },
+            u'Timeline': {
+                u'CreationDateTime': datetime.datetime(2016, 7, 13, 10, 0, 0, 576000, tzinfo=tzlocal()),
+                u'EndDateTime': datetime.datetime(2016, 7, 13, 10, 45, 48, 205000, tzinfo=tzlocal()),
+                u'ReadyDateTime': datetime.datetime(2016, 7, 13, 10, 4, 6, 25000, tzinfo=tzlocal())
+            }
+        }
+    }
+}
+
+
+
+TERMINATED_WITH_ERRORS_CLUSTER_STATUS_RESPONSE = {
+    u'Cluster': {
+        u'Id': u'A Cluster ID',
+        u'Status': {
+            u'State': u'TERMINATED_WITH_ERRORS',
+            u'StateChangeReason': {
+                u'Code': u'STEP_FAILURE',
+                u'Message': u'Shut down as step failed'
+            },
+            u'Timeline': {
+                u'CreationDateTime': datetime.datetime(2016, 7, 13, 5, 52, 5, 368000, tzinfo=tzlocal()),
+                u'EndDateTime': datetime.datetime(2016, 7, 13, 7, 43, 59, 890000, tzinfo=tzlocal()),
+                u'ReadyDateTime': datetime.datetime(2016, 7, 13, 5, 56, 11, 219000, tzinfo=tzlocal())
+            }
+        }
+    }
+}
+
+
+def test_has_cluster_successfully_finished():
+    emr_mock = MagicMock()
+    attrs = {'describe_cluster':
+                 (lambda ClusterId: TERMINATED_CLUSTER_STATUS_RESPONSE)}
+    emr_mock.configure_mock(**attrs)
+
+    assert has_cluster_successfully_finished(emr_mock, TEST_CLUSTER_ID) == True
+
+
+def test_has_cluster_successfully_finished_not():
+    emr_mock = MagicMock()
+    attrs = {'describe_cluster':
+                 (lambda ClusterId: TERMINATED_WITH_ERRORS_CLUSTER_STATUS_RESPONSE)}
+    emr_mock.configure_mock(**attrs)
+
+    with raises(EMRPollingException):
+        has_cluster_successfully_finished(emr_mock, TEST_CLUSTER_ID)
+
+
+def test_has_reached_wait_limit():
+    emr_mock = MagicMock()
+    attrs = {'describe_cluster':
+                 (lambda ClusterId: RUNNING_CLUSTER_STATUS_RESPONSE)}
+    emr_mock.configure_mock(**attrs)
+
+    with raises(EMRPollingException):
+         has_cluster_successfully_finished(emr_mock, TEST_CLUSTER_ID)
+
