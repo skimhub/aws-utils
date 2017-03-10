3fb8f9e1a89d8936850f42d91185e7dd19d62be0
Alasdair Ellis <alasdair@skimlinks.com>
2017-03-10 11:42:48 +0000

    fix: restructure requirements so we dont install testing requirements by default

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
 from setuptools import setup, find_packages
 
 package = 'aws-utils'
-version = '0.2.6'
+version = '0.2.7'
 
 INSTALL_REQUIRES = [
-    'boto>=2.38.0,<3.0.0',
-    'boto3>=1.2.3,<1.3.0',
-    'smart-open>=1.3.1,<1.4.0',
+    'boto>=2.38.0',
+    'boto3>=1.2.3',
+    'smart-open>=1.3.1',
     'dateutils>=0.6.6',
     'retrying>=1.3.3'
-]
-
-TEST_REQUIRES = [
-    'moto>=0.4.19,<0.5.0',
-    'pytest>=2.8.5,<2.9.0',
-    'mock==1.3.0'
 ]
 
 setup(
@@ -25,6 +19,5 @@ setup(
     description="collection of AWS useful functions",
     url='https://github.com/skimhub/aws-utils',
     packages=find_packages(exclude=['test']),
-    install_requires=INSTALL_REQUIRES + TEST_REQUIRES, # not entirely corect but gets tests with moto working
-    # test_require=TEST_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
 )
