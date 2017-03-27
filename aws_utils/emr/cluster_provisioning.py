ac92e7d7377d4cba4658448a1f34e0b7923e548c
daniel <daniel.slater@skimlinks.com>
2017-03-27 13:54:13 +0100

    paramaterize billing

--- aws_utils/emr/cluster_provisioning.py
+++ aws_utils/emr/cluster_provisioning.py
@@ -1,6 +1,5 @@
 import copy
 
-
 INSTANCE = {
     'InstanceGroups': [
         {
@@ -43,81 +42,89 @@ CONFIGURATIONS = [
 
 BOOTSTRAP_ACTION = {
 
-        'Name': '',  # name of bootstrap action
-        'ScriptBootstrapAction': {
-            'Path': '',  # path to script runner that executes bootstrap action
-            'Args': [
-                '',  # package version
-            ]
-        }
+    'Name': '',  # name of bootstrap action
+    'ScriptBootstrapAction': {
+        'Path': '',  # path to script runner that executes bootstrap action
+        'Args': [
+            '',  # package version
+        ]
+    }
 }
 
-CLUSTER = {
-    'Name': '',  # name of the EMR cluster
-    'VisibleToAllUsers': True,
-    'ReleaseLabel': 'emr-4.6.0',
-    'JobFlowRole': 'EMR_EC2_DefaultRole',
-    'ServiceRole': 'EMR_DefaultRole',
-    'LogUri': '',  # EMR log uri
-    'Applications': [{'Name': 'Spark'}, {'Name': 'Ganglia'}],
-    'Tags': [
-                {'Key': 'billing', 'Value': 'audience'},
-                {'Key': 'billing_secondary', 'Value': 'audience'},
-            ],
-    'Instances': '',  # instance config
-    'Configurations': '',  # configuration config
-    'BootstrapActions': '',  # bootstrap config
-    'Steps': '',  # steps config
-}
+
+def _get_cluster_config(name, billing, billing_secondary, billing_tag, log_uri, configurations_config, instance_template):
+    return {
+        'Name': name,  # name of the EMR cluster
+        'VisibleToAllUsers': True,
+        'ReleaseLabel': 'emr-4.6.0',
+        'JobFlowRole': 'EMR_EC2_DefaultRole',
+        'ServiceRole': 'EMR_DefaultRole',
+        'LogUri': log_uri,  # EMR log uri
+        'Applications': [{'Name': 'Spark'}, {'Name': 'Ganglia'}],
+        'Tags': [
+            {'Key': 'billing', 'Value': billing},
+            {'Key': 'billing_secondary', 'Value': billing_secondary},
+            {'Key': 'tier', 'Value': billing_tag}
+        ],
+        'Instances': instance_template,  # instance config
+        'Configurations': configurations_config,  # configuration config
+        'BootstrapActions': '',  # bootstrap config
+        'Steps': '',  # steps config
+    }
+
 
 STEP = {
-        'Name': '',
-        'ActionOnFailure': 'TERMINATE_CLUSTER',
-        'HadoopJarStep': {
+    'Name': '',
+    'ActionOnFailure': 'TERMINATE_CLUSTER',
+    'HadoopJarStep': {
 
-            'Jar': 's3://elasticmapreduce/libs/script-runner/script-runner.jar',
-            'MainClass': '',
-            'Args': [
-                '',  # path to executor
-                '',  # command that will be executed
-            ]
-        }
+        'Jar': 's3://elasticmapreduce/libs/script-runner/script-runner.jar',
+        'MainClass': '',
+        'Args': [
+            '',  # path to executor
+            '',  # command that will be executed
+        ]
+    }
 }
 
 
-def construct_base_cluster(instances, instance_type, bid_price, vpc_subnet, cluster_name, tag, cluster_log_uri, on_demand=None, keep_alive=None):
+def construct_base_cluster(instances, instance_type, bid_price, vpc_subnet, cluster_name, billing_tag, cluster_log_uri,
+                           billing, billing_secondary,
+                           on_demand=None,
+                           keep_alive=None):
     """Returns a completed boto3 cluster configuration without steps.
 
     Args:
-        instances, int:
+        billing (str):
+        billing_secondary (str):
+        instances (int):
             number of core instances the cluster should have.
-        instance_type, str:
+        instance_type (str):
             ec2 instance type master and core instances.
-        bid_price, str:
+        bid_price (str):
             bid price for the EC2 instances within the cluster, core and
             master.
-        vpc_subnet, str:
+        vpc_subnet (str):
             VPC subnet the EMR cluster should be hosted in.
-        cluster_name, str:
+        cluster_name (str):
             Name of the cluster.
-        tag, str:
+        billing_tag (str):
             AWS billing tag to be associated with the cluster, 'live' or 'dev'.
-        cluster_log_uri, str:
+        cluster_log_uri (str):
             S3 uri to where the EMR logs will be stored.
-        on_demand, boolean:
+        on_demand (boolean):
             whether we should use on demand instances rather than spot.
-        keep_alive, boolean:
+        keep_alive (boolean):
             whether the cluster should remain alive without steps/upon
             failure of a step.
     Returns:
-        cluster_config, dict:
+        dict :
             boto3 config ready to be submitted to AWS.
             Does not include bootstrap or steps.
     """
     # Load the templates
     instance_template = copy.deepcopy(INSTANCE)
     configurations_config = copy.deepcopy(CONFIGURATIONS)
-    cluster_config_template = copy.deepcopy(CLUSTER)
 
     # Update master and core instance values
     master_instance_config, core_instance_config = instance_template['InstanceGroups']
@@ -139,15 +146,10 @@ def construct_base_cluster(instances, instance_type, bid_price, vpc_subnet, clus
         instance_template['KeepJobFlowAliveWhenNoSteps'] = True
 
     # Update general cluster settings
-    cluster_config_template['Name'] = cluster_name
-    cluster_config_template['LogUri'] = cluster_log_uri
-    cluster_config_template['Tags'].append({'Key': 'tier', 'Value': tag})
+    cluster_config = _get_cluster_config(cluster_name, billing, billing_secondary, billing_tag, cluster_log_uri, configurations_config,
+                                         instance_template)
 
-    # Add completed Instance and Configurations to the config template
-    cluster_config_template['Instances'] = instance_template
-    cluster_config_template['Configurations'] = configurations_config
-
-    return cluster_config_template
+    return cluster_config
 
 
 def construct_bootstrap_action(bootstrap_name, bootstrap_path, bootstrap_args):
@@ -173,43 +175,6 @@ def construct_bootstrap_action(bootstrap_name, bootstrap_path, bootstrap_args):
     bootstrap_action_template['ScriptBootstrapAction']['Path'] = bootstrap_path
 
     return bootstrap_action_template
-
-
-def construct_cli_step(script_args, script_path, exec_path, script_name, keep_alive):
-    """Returns a CLI step for a cluster.
-
-    Args:
-        script_args, list:
-            comma separated script arguments
-        script_path, str:
-            path to the spark script to be run, relative to the top
-            of the repo.
-        exec_path, str:
-            S3 path to the script that runs the step.
-        script_name, str:
-            name of the step, used for descriptive purpose on the EMR console
-        keep_alive, boolean:
-            whether we should keep the cluster alive in the event of
-            the step failing.
-
-    Returns:
-        step, dict:
-            a boto3 spark step for a AWS EMR cluster
-    """
-    # Load the template
-    step_template = copy.deepcopy(STEP)
-
-    # Construct the script argument
-#    step_args = [script_path] + script_args
-    step = ' '.join(str(arg) for arg in script_args)
-
-    # Update required values
-    step_template['Name'] = script_name
-    step_template['HadoopJarStep']['Args'] = [exec_path, step]
-    if keep_alive:
-        step_template['ActionOnFailure'] = 'CANCEL_AND_WAIT'
-
-    return step_template
 
 
 def construct_spark_step(spark_args, script_args, script_path, exec_path, step_name, keep_alive):
