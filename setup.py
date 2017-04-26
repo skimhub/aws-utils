3695eb27d4314858029233d823634016b34cbf15
Ali <alasdair@skimlinks.com>
2017-04-26 16:22:57 +0100

    fix: bump smart open version to solve the boto host issue

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.4.2'
 INSTALL_REQUIRES = [
     'boto>=2.38.0',# need to stay in this version as sqs.get_queue function stops working when we upgrade
     'boto3>=1.2.3',
-    'smart_open==1.3.2',# smart open must be 1.3.2 because in 1.3.3 onward the gzip write functionality has been removed
+    'smart_open==1.3.3',# smart open must be 1.3.3 because later versions break out merge part files function in data-store mappers
     'dateutils>=0.6.6',
     'retrying>=1.3.3'
 ]
